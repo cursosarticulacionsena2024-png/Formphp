48a456decb2faa9e594b641f797265b2e897eeb5
cursosarticulacionsena2024-png <cursosarticulacionsena2024@gmail.com>
2025-08-08 14:36:41 -0500

    informacion.php

---
+++ informacion.php
@@ -0,0 +1,10 @@
+<?php
+$Identificacion= $_post ["Identificacion"];
+$NombreApellidoS=$_post ["NombreApellido];
+$Cargo=$_post ["Cargo"];
+$Salario=$_post ["Salario"];
+echo "la identificacion del empleado es: " .Identificacion;
+echo "Los Nombres Y apellidos del Empleado son: ".NombreApellido;
+echo "el cargo del empleado es: " .Cargo;
+echo "el salario del empleado es: " .Salario;
+?>
